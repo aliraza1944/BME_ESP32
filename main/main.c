82404f0b133f57c937a6e2d2bd162d7fd4144815
aliraza1944 <aliraza1944@gmail.com>
2017-09-20 01:37:45 +0500

    1

--- main/main.c
+++ main/main.c
@@ -32,7 +32,7 @@ I2C Definitions
  #define I2C_MASTER_NUM   I2C_NUM_1   /*!< I2C port number for master dev */
  #define I2C_MASTER_TX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
  #define I2C_MASTER_RX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
- #define I2C_MASTER_FREQ_HZ    10000     /*!< I2C master clock frequency */
+ #define I2C_MASTER_FREQ_HZ    200000     /*!< I2C master clock frequency */
 
  #define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
  #define READ_BIT   I2C_MASTER_READ  /*!< I2C master read */
