1db50b4c77210e227bd598984fc366cffc944618
aliraza1944 <aliraza1944@gmail.com>
2017-10-05 12:11:52 +0500

    1

--- main/main.c
+++ main/main.c
@@ -57,7 +57,7 @@ I2C Definitions
 
  #define I2C_MASTER_SCL_IO    19    /*!< gpio number for I2C master clock */
  #define I2C_MASTER_SDA_IO    18    /*!< gpio number for I2C master data  */
- #define I2C_MASTER_NUM   I2C_NUM_1   /*!< I2C port number for master dev */
+ #define I2C_MASTER_NUM   I2C_NUM_0   /*!< I2C port number for master dev */
  #define I2C_MASTER_TX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
  #define I2C_MASTER_RX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
  #define I2C_MASTER_FREQ_HZ    100000     /*!< I2C master clock frequency */
